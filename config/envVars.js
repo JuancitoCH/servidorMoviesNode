2fefbf7e308bb39012905fa0b5aa0461fd5f10d9
juancitoCH <cjuan.chona@hotmail.com>
2022-02-14 02:17:05 -0300

    aaaaaa

--- config/envVars.js
+++ config/envVars.js
@@ -6,7 +6,10 @@ const varConfig = {
     db_password: process.env.DB_PASSWORD,
     db_host : process.env.DB_HOST,
     db_name: process.env.DB_NAME,
-    jwt_secret: process.env.JWT_SECRET
+    jwt_secret: process.env.JWT_SECRET,
+    oauth_client_id:process.env.OAUTH_CLIENT_ID,
+    oauth_client_secret:process.env.OAUTH_CLIENT_SECRET,
+    oauth_callback_url:process.env.OAUTH_CALLBACK_URL
 }
 
 module.exports = varConfig
